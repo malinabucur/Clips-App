b0f317a97f3cf98e14409263e9e38daa8489785b
malinabucur <malinabucur13@gmail.com>
2023-11-09 13:49:23 +0200

    imports

--- applicationFE/src/app/shared/shared.module.ts
+++ applicationFE/src/app/shared/shared.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-// import { ModalService } from '../services/modal.service';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabComponent, TabsContainerComponent],
-  // providers: [
-  //    ModalService,
-  // ]
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
