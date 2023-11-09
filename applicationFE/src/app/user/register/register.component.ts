e2324372da1020da1881bd72a7f78bc9226b6bf8
malinabucur <malinabucur13@gmail.com>
2023-11-09 13:50:12 +0200

    biding form controls

--- applicationFE/src/app/user/register/register.component.ts
+++ applicationFE/src/app/user/register/register.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [Validators.required, Validators.min(18)]);
+  password = new FormControl('', [Validators.required, Validators.min(8)]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(10),
+    Validators.maxLength(10),
+  ]);
 
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    passpassword: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
+  });
 }
