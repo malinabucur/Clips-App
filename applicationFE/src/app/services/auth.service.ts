4b60045d8ffcbaad94ee343943d78fec86494afe
malinabucur <malinabucur13@gmail.com>
2023-11-17 15:14:00 +0200

    add delay for closing modal (delay pipeable operator)

--- applicationFE/src/app/services/auth.service.ts
+++ applicationFE/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -14,10 +14,12 @@ import { map } from 'rxjs/operators';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
