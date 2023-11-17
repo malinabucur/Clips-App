ec9b84e26f5aff109fb45e9cec75ca7eb66f967c
malinabucur <malinabucur13@gmail.com>
2023-11-17 14:41:41 +0200

    show links based on isAuthenticated property

--- applicationFE/src/app/services/auth.service.ts
+++ applicationFE/src/app/services/auth.service.ts
@@ -5,15 +5,19 @@ import {
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
   }
 
   public async createUser(userData: IUser) {
