cffdc8d9dd23baceafc38ebddc6e498388eab7c8
malinabucur <malinabucur13@gmail.com>
2023-11-20 12:29:02 +0200

    add custom register validator class (for password and confirm_password)

--- applicationFE/src/app/user/register/register.component.ts
+++ applicationFE/src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -31,14 +32,17 @@ export class RegisterComponent {
   alertMessage = 'Please wait! Your account is being created!';
   alertColor = 'blue';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
