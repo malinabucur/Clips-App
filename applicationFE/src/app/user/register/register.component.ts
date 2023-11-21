c58eab34eb70d7c79b3e0c059c4c64fcf59a08c4
malinabucur <malinabucur13@gmail.com>
2023-11-21 17:51:52 +0200

    create email-taken component to check if the email is already registered

--- applicationFE/src/app/user/register/register.component.ts
+++ applicationFE/src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
 import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
+import { FirebaseError } from 'firebase/app';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +12,16 @@ import { RegisterValidators } from '../validators/register-validators';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   inSubmission = false;
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl('', [Validators.required, Validators.min(18)]);
   password = new FormControl('', [
     Validators.required,
@@ -53,10 +59,19 @@ export class RegisterComponent {
     try {
       await this.auth.createUser(this.registerForm.value as IUser);
     } catch (e) {
-      console.log(e);
+      console.log('Registration error: ', e);
 
-      this.alertMessage =
-        'An unexpected error occurred. Please try again later!';
+      if (
+        e instanceof FirebaseError &&
+        e.code === 'auth/email-already-in-use'
+      ) {
+        this.alertMessage =
+          'This email address is already registered. Please try another one!';
+      } else {
+        this.alertMessage =
+          'An unexpected error ocurred. Please try again later!';
+      }
+
       this.alertColor = 'red';
       this.inSubmission = false;
       return;
