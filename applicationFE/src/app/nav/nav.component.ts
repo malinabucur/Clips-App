0813086baa0f365e7aa1af6456c277fabc8264ac
malinabucur <malinabucur13@gmail.com>
2023-11-22 13:34:04 +0200

    move logout method in a service for reusability

--- applicationFE/src/app/nav/nav.component.ts
+++ applicationFE/src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -9,11 +8,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(
-    public modal: ModalService,
-    public auth: AuthService,
-    private afAuth: AngularFireAuth
-  ) {}
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   ngOnInit(): void {}
 
@@ -21,11 +16,5 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
 
     this.modal.toggleModal('auth');
-  }
-
-  async logout($event: Event) {
-    $event.preventDefault();
-
-    await this.afAuth.signOut();
   }
 }
--- applicationFE/src/app/services/auth.service.ts
+++ applicationFE/src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import {
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
 import { map, delay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,11 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
@@ -44,5 +49,15 @@ export class AuthService {
     });
 
     await userCred.user.updateProfile({ displayName: userData.name });
+  }
+
+  public async logout($event: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+
+    await this.auth.signOut();
+
+    await this.router.navigateByUrl('/');
   }
 }
