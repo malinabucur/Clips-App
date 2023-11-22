a9156a86cc3d2c3870d0ae4aef8991d52766017e
malinabucur <malinabucur13@gmail.com>
2023-11-22 16:06:03 +0200

    add query param for sorting url

--- applicationFE/src/app/video/manage/manage.component.ts
+++ applicationFE/src/app/video/manage/manage.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
 })
-export class ManageComponent {}
+export class ManageComponent implements OnInit {
+  videoOrder = '1';
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? '2' : '1';
+    });
+  }
+
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
+
+    this.router.navigateByUrl(`/manage?sort=${value}`);
+  }
+}
