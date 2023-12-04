34d18729b0d09be9d01d3b2d283a3029b80325ee
malinabucur <malinabucur13@gmail.com>
2023-12-04 17:10:44 +0200

    create openModal method

--- applicationFE/src/app/video/manage/manage.component.ts
+++ applicationFE/src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -15,7 +16,8 @@ export class ManageComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -43,5 +45,11 @@ export class ManageComponent implements OnInit {
         sort: value,
       },
     });
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.modal.toggleModal('editClip');
   }
 }
